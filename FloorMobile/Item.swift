96aff4c1578838b15fff35108b2e7fadec16f234
Elie Buff <elie.buff@levana.io>
2026-09-07 11:11:06 +0200

    Initial Commit

---
+++ FloorMobile/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FloorMobile
+//
+//  Created by elie buff on 07/09/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ FloorMobileTests/FloorMobileTests.swift
@@ -0,0 +1,19 @@
+//
+//  FloorMobileTests.swift
+//  FloorMobileTests
+//
+//  Created by elie buff on 07/09/2026.
+//
+
+import Testing
+@testable import FloorMobile
+
+struct FloorMobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
